9b3c3a8cd0d680113a316fceaf23a09828216b67
Dr. Jordan Jordanov <d_yugioh@abv.bg>
2026-03-25 12:31:24 +0200

    Implement GET Slido endpoint and POST Slido comments
    Implement GET Slido endpoint and POST Slido comments

--- StudentsManager.Mvc/Controllers/SlidoController.cs
+++ StudentsManager.Mvc/Controllers/SlidoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using StudentsManager.Mvc.Domain.Inputs.Forum;
 using StudentsManager.Mvc.Services.Forum;
 
 namespace StudentsManager.Mvc.Controllers
@@ -7,6 +8,26 @@ namespace StudentsManager.Mvc.Controllers
     [ApiController]
     public class SlidoController(IForumService service) : ControllerBase
     {
+        // GET: api/slido?limit=20&skip=0
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] int limit, [FromQuery] int skip)
+        {
+            var result = await service.GetAsync(limit, skip);
+            return Ok(result);
+        }
+
+        // POST: api/slido/comment
+        [HttpPost("comment")]
+        public async Task<IActionResult> PostComment([FromBody] Comment input)
+        {
+            if (string.IsNullOrWhiteSpace(input.Description) || input.ForumQuestionId == 0)
+                return BadRequest("Comment cannot be empty");
+
+            await service.SaveCommentAsync(input,Guid.Parse("1eac9820-5e6e-4d10-6e94-08de36f40f78"));
+
+            return Ok("Comment posted successfully");
+        }
+        
         // GET: api/slido/questions?limit=20&skip=0
         [HttpGet("questions")]
         public async Task<IActionResult> GetQuestions([FromQuery] int limit, [FromQuery] int skip)
